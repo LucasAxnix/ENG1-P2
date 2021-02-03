c0776bdea367c449bcf0077487c79534ec15a87c
oliverTaylor12 <72559308+oliverTaylor12@users.noreply.github.com>
2021-02-03 15:02:25 +0000

    Added test files for all classes
    included sample tests for clarity of formatting for future reference

---
+++ core/src/test/java/com/hardgforgif/dragonboatracing/SaveGame/SaveTest.java
@@ -0,0 +1,13 @@
+package com.hardgforgif.dragonboatracing.SaveGame;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SaveTest {
+    @Test
+    public void aPassedTest() throws Exception{
+        System.out.println("Hello, test has run!");
+        assertEquals(0,0);
+    }
+}
